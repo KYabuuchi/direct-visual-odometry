88f2d2a436492765134356497c679b95ec85e33f
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2019-03-08 15:51:46 +0900

    se3-test

--- test/se3.cpp
+++ test/se3.cpp
@@ -1,34 +1,49 @@
 #include "math.hpp"
-#include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/viz.hpp>
 
+bool loop = true;
 int main()
 {
     using namespace math;
-    {
-        cv::Mat1f mat(cv::Mat1f::eye(4, 4));
-        std::cout << mat << std::endl;
+    // viz準備
+    cv::viz::Viz3d viz_window("3D-VIEW");
+    viz_window.showWidget("coordinate", cv::viz::WCoordinateSystem(0.5));
+    viz_window.registerKeyboardCallback([](const cv::viz::KeyboardEvent&, void*) -> void { loop = false; }, &viz_window);
 
-        mat = se3::log(mat);
-        std::cout << mat << std::endl;
+    // randomな回転行列の生成
+    cv::Mat1f seed(6, 1);
+    cv::randu(seed, 1.0f, 3.0f);
+    cv::Mat1f T = se3::exp(seed);
+    cv::Mat1f xi = se3::log(T);
 
-        mat = se3::exp(mat);
-        std::cout << mat << std::endl;
+    // 座標変換
+    cv::Point3f origin(0, 0, 0);
+    cv::Mat1f before = (cv::Mat1f(4, 1) << 0, 0, 1, 1);
+    cv::Mat1f after(T * before);
+
+    // 移動前(黒)，移動後(白)
+    cv::viz::WArrow arrow_b(origin, cv::Point3f(before.rowRange(0, 3)), 0.01, cv::viz::Color::black());
+    cv::viz::WArrow arrow_a(cv::Point3f(T.col(3).rowRange(0, 3)), cv::Point3f(after.rowRange(0, 3)), 0.01, cv::viz::Color::white());
+    viz_window.showWidget("before", arrow_b);
+    viz_window.showWidget("after", arrow_a);
+
+    // 補完(黄)
+    for (int i = 1; i < 10; i++) {
+        cv::Mat1f tmp = se3::exp(xi * static_cast<float>(i) / 10.0);
+        cv::Mat1f inter(tmp * before);
+        cv::viz::WArrow arrow_i(cv::Point3f(tmp.col(3).rowRange(0, 3)), cv::Point3f(inter.rowRange(0, 3)), 0.01, cv::viz::Color::yellow());
+        viz_window.showWidget("inter" + std::to_string(i), arrow_i);
     }
-    std::cout << "==== test1 done ====\n"
-              << std::endl;
-
-    {
-        cv::Mat1f mat(6, 1);
-        cv::randu(mat, -10.0f, 10.0f);
-        std::cout << mat << std::endl;
-        mat = se3::exp(mat);
-        std::cout << mat << std::endl;
-        mat = se3::log(mat);
-        std::cout << mat << std::endl;
-        mat = se3::exp(mat);
-        std::cout << mat << std::endl;
+    // 予測(緑)
+    for (int i = 1; i < 10; i++) {
+        cv::Mat1f tmp = se3::exp(xi * static_cast<float>(10.0 + i) / 10.0);
+        cv::Mat1f predict(tmp * before);
+        cv::viz::WArrow arrow_p(cv::Point3f(tmp.col(3).rowRange(0, 3)), cv::Point3f(predict.rowRange(0, 3)), 0.01, cv::viz::Color::lime());
+        viz_window.showWidget("predict" + std::to_string(i), arrow_p);
     }
-    std::cout << "==== test2 done ====\n"
-              << std::endl;
+
+    while (loop) {
+        viz_window.spinOnce(1, true);
+    }
 }

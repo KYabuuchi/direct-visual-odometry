3cbba35a6758680013c84402adb76feaf4d41853
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2019-03-12 13:09:41 +0900

    debug

--- test/tracking.cpp
+++ test/tracking.cpp
@@ -70,6 +70,8 @@ int main(int argc, char* argv[])
         // num2 = tmp > 0 ? tmp : 0;
     }
 
+    std::cout << num1 << " " << num2 << std::endl;
+
     // prepare depth-image & gray-image
 #ifndef DEBUGG
     io::Loader loader("../data/KINECT_5MM/info.txt");
@@ -119,76 +121,79 @@ int main(int argc, char* argv[])
     cv::Mat1f B(cv::Mat1f::zeros(0, 1));
     cv::Mat1f xi = math::se3::log(cv::Mat1f::eye(4, 4));
 
-    for (int iteration = 0; iteration < 10; iteration++) {
+    for (int level = 0; level < 5; level++) {
+        int skip = 20 * (5 - level);
+        std::cout << "SKIP " << skip << std::endl;
+        for (int iteration = 0; iteration < 10; iteration++) {
+            float residual = 0;
+            for (int col = 0; col < COL; col += skip) {
+                for (int row = 0; row < ROW; row += skip) {
+                    cv::Point2f x_i0 = cv::Point2f(col, row);
+                    float depth = depth_image2.at<float>(x_i0);
+                    if (depth < 0.01)  // 1e-6[m]
+                        continue;
 
-        float residual = 0;
-        for (int col = 0; col < COL; col += 80) {
-            for (int row = 0; row < ROW; row += 80) {
-                cv::Point2f x_i0 = cv::Point2f(col, row);
-                float depth = depth_image2.at<float>(x_i0);
-                if (depth < 0.01)  // 1e-6[m]
-                    continue;
+                    // calc residual
+                    cv::Point2f x_i = warp(xi, x_i0, depth);
+                    float a = Converter::getColorSubpix(gray_image2, x_i);
+                    float b = gray_image1.at<float>(x_i0);
+                    float r = a - b;
+                    residual += r * r;
 
-                // calc residual
-                cv::Point2f x_i = warp(xi, x_i0, depth);
-                float a = Converter::getColorSubpix(gray_image2, x_i);
-                float b = gray_image1.at<float>(x_i0);
-                float r = a - b;
-                residual += r * r;
+                    if (r == 0)
+                        continue;
 
-                if (r == 0)
-                    continue;
-
-                // clang-format off
+                    // clang-format off
                 cv::Mat1f jacobian1 = (cv::Mat1f(1, 2) << 
                     gradient_image_x.at<float>(x_i),
                     gradient_image_y.at<float>(x_i));
-                // clang-format on
+                    // clang-format on
 
-                cv::Point3f x_c = cv::Point3f(Converter::backProject(Params::KINECTV2_INTRINSIC_DEPTH, cv::Mat1f(x_i0), depth));
+                    cv::Point3f x_c = cv::Point3f(Converter::backProject(Params::KINECTV2_INTRINSIC_DEPTH, cv::Mat1f(x_i0), depth));
 #ifdef DEBUG3
-                cv::Mat1f jacobian2 = cv::Mat1f::zeros(2, 3);
+                    cv::Mat1f jacobian2 = cv::Mat1f::zeros(2, 3);
 #else
-                cv::Mat1f jacobian2 = cv::Mat1f::zeros(2, 6);
+                    cv::Mat1f jacobian2 = cv::Mat1f::zeros(2, 6);
 #endif
-                {
-                    const Camera& c = camera;
-                    float x = x_c.x;
-                    float y = x_c.y;
-                    float z = x_c.z;
-                    jacobian2(0, 0) = c.fx / z;
-                    jacobian2(0, 1) = 0;
-                    jacobian2(0, 2) = -c.fx * x / z / z;
-                    jacobian2(1, 0) = 0;
-                    jacobian2(1, 1) = c.fy / z;
-                    jacobian2(1, 2) = -c.fy * y / z / z;
+                    {
+                        const Camera& c = camera;
+                        float x = x_c.x;
+                        float y = x_c.y;
+                        float z = x_c.z;
+                        jacobian2(0, 0) = c.fx / z;
+                        jacobian2(0, 1) = 0;
+                        jacobian2(0, 2) = -c.fx * x / z / z;
+                        jacobian2(1, 0) = 0;
+                        jacobian2(1, 1) = c.fy / z;
+                        jacobian2(1, 2) = -c.fy * y / z / z;
 #ifndef DEBUG3
-                    jacobian2(0, 3) = -c.fx * x * y / z / z;
-                    jacobian2(0, 4) = c.fx * (1 + x * x / z / z);
-                    jacobian2(0, 5) = -c.fx * y / z;
-                    jacobian2(1, 3) = -c.fy * (1 + y * y / z / z);
-                    jacobian2(1, 4) = c.fy * x * y / z / z;
-                    jacobian2(1, 5) = c.fy * x / z;
+                        jacobian2(0, 3) = -c.fx * x * y / z / z;
+                        jacobian2(0, 4) = c.fx * (1 + x * x / z / z);
+                        jacobian2(0, 5) = -c.fx * y / z;
+                        jacobian2(1, 3) = -c.fy * (1 + y * y / z / z);
+                        jacobian2(1, 4) = c.fy * x * y / z / z;
+                        jacobian2(1, 5) = c.fy * x / z;
 #endif
+                    }
+
+                    // stack coefficient
+                    cv::vconcat(A, jacobian1 * jacobian2, A);
+                    cv::vconcat(B, cv::Mat1f(cv::Mat1f(1, 1) << r), B);
                 }
-
-                // stack coefficient
-                cv::vconcat(A, jacobian1 * jacobian2, A);
-                cv::vconcat(B, cv::Mat1f(cv::Mat1f(1, 1) << r), B);
             }
-        }
 
-        cv::Mat update;
-        // A xi = - B
-        cv::solve(A, -B, update, cv::DECOMP_SVD);
-        // std::cout << A << std::endl;
+            cv::Mat update;
+            // A xi = - B
+            cv::solve(A, -B, update, cv::DECOMP_SVD);
+            // std::cout << A << std::endl;
 
 #ifndef DEBUG3
-        xi = math::se3::concatenate(xi, update);
-        std::cout << xi.t() << std::endl;
+            xi = math::se3::concatenate(xi, update);
+            std::cout << xi.t() << std::endl;
 #endif
-        std::cout << residual << std::endl;
-        std::cout << update.t() << std::endl;
+            std::cout << residual << std::endl;
+            std::cout << update.t() << std::endl;
+        }
     }
     std::cout << "\n"
               << math::se3::exp(xi) << std::endl;

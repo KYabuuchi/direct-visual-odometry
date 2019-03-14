e5b5aae826c29312cdc044e71b7b33aee466a13a
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2019-03-15 00:17:02 +0900

    frame

--- test/tracking.cpp
+++ test/tracking.cpp
@@ -1,5 +1,6 @@
-// 相対姿勢の計算
+// 相対姿勢の計算(重みなし)
 #include "converter.hpp"
+#include "frame.hpp"
 #include "io.hpp"
 #include "math.hpp"
 #include "matplotlibcpp.h"
@@ -8,73 +9,15 @@
 #include <cassert>
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
-class Frame
-{
-public:
-    Frame() {}
-    cv::Mat depth_image1;
-    cv::Mat depth_image2;
-    cv::Mat gray_image1;
-    cv::Mat gray_image2;
-    cv::Mat1f intrinsic_depth;
-    cv::Mat1f intrinsic_rgb;
-    int cols;
-    int rows;
-};
-
-std::vector<Frame> createFramePyramid(
-    const int level,
-    const cv::Mat& intrinsic_depth,
-    const cv::Mat& intrinsic_rgb,
-    const cv::Mat& gray_image1,
-    const cv::Mat& depth_image1,
-    const cv::Mat& gray_image2,
-    const cv::Mat& depth_image2)
-{
-    std::vector<Frame> frames(level);
-
-    cv::Mat leveled_gray_image1 = gray_image1;
-    cv::Mat leveled_gray_image2 = gray_image2;
-    cv::Mat leveled_depth_image1 = depth_image1;
-    cv::Mat leveled_depth_image2 = depth_image2;
-
-    auto pow2 = [](int num) -> int {int n=1;for(int i=0;i<num;i++)n*=2; return n; };
-
-    for (int i = level - 1; i >= 0; i--) {
-        int scale = pow2(level - 1 - i);
-        frames.at(i).intrinsic_depth = intrinsic_depth / scale;
-        frames.at(i).intrinsic_depth(2, 2) = 1;
-        frames.at(i).intrinsic_rgb = intrinsic_rgb / scale;
-        frames.at(i).intrinsic_rgb(2, 2) = 1;
-
-        frames.at(i).depth_image1 = leveled_depth_image1.clone();
-        frames.at(i).depth_image2 = leveled_depth_image2.clone();
-        frames.at(i).gray_image1 = leveled_gray_image1.clone();
-        frames.at(i).gray_image2 = leveled_gray_image2.clone();
-
-        frames.at(i).rows = leveled_depth_image1.rows;
-        frames.at(i).cols = leveled_depth_image1.cols;
-
-        leveled_depth_image1 = Converter::cullImage(leveled_depth_image1);
-        leveled_depth_image2 = Converter::cullImage(leveled_depth_image2);
-        leveled_gray_image1 = Converter::cullImage(leveled_gray_image1);
-        leveled_gray_image2 = Converter::cullImage(leveled_gray_image2);
-    }
-
-    return frames;
-}
-
 // #define DEBUG
 
 int main(int argc, char* argv[])
 {
-    int num1 = 0, num2 = 5;
+    int num1 = 0, num2 = 10;
     if (argc == 2)
-        num1 = std::atoi(argv[1]) + 5;
+        num1 = std::atoi(argv[1]) + 10;
     std::cout << num1 << " " << num2 << std::endl;
-    assert(0 <= num1 and num1 <= 16);
-
+    assert(0 <= num1 and num1 <= 20);
 
     // prepare depth-image & gray-image
     io::Loader loader("../data/KINECT_1DEG/info.txt");
@@ -105,109 +48,77 @@ int main(int argc, char* argv[])
 
     // image window
     cv::namedWindow("gray", cv::WINDOW_NORMAL);
-    cv::namedWindow("depth", cv::WINDOW_NORMAL);
-    cv::namedWindow("grad", cv::WINDOW_NORMAL);
+    cv::namedWindow("depth&grad", cv::WINDOW_NORMAL);
+    // cv::namedWindow("grad", cv::WINDOW_NORMAL);
 
     cv::Mat1f xi(cv::Mat1f::zeros(6, 1));
     std::vector<std::vector<float>> vector_of_residuals;
 
     // create frame pyramid
-    std::vector<Frame> frames = createFramePyramid(
-        5,
-        Params::KINECTV2_INTRINSIC_DEPTH,
-        Params::KINECTV2_INTRINSIC_RGB,
-        gray_image1, depth_image1,
-        gray_image2, depth_image2);
-
+    std::vector<Frame> pre_frames = createFramePyramid(
+        depth_image1, gray_image1,
+        Params::KINECTV2_INTRINSIC_DEPTH, 5);
+    std::vector<Frame> cur_frames = createFramePyramid(
+        depth_image2, gray_image2,
+        Params::KINECTV2_INTRINSIC_DEPTH, 5);
 
     for (int level = 0; level < 3; level++) {
-        Frame frame = frames.at(level);
-        const int ROW = frame.rows;
-        const int COL = frame.cols;
+        const Frame& pre_frame = pre_frames.at(level);
+        const Frame& cur_frame = cur_frames.at(level);
+        const int ROW = pre_frame.m_rows;
+        const int COL = pre_frame.m_cols;
         std::cout << "\nLEVEL: " << level << " ROW: " << ROW << " COL: " << COL << std::endl;
 
         // gradient gray_image
-        cv::Mat gradient_image_x = Converter::gradiate(frame.gray_image2, true);
-        cv::Mat gradient_image_y = Converter::gradiate(frame.gray_image2, false);
+        cv::Mat gradient_image_x = Converter::gradiate(cur_frame.m_gray_image, true);
+        cv::Mat gradient_image_y = Converter::gradiate(cur_frame.m_gray_image, false);
 
         // vector of residual
         std::vector<float> residuals;
-
-        // show image
-        {
-            cv::Mat show_image;
-            cv::hconcat(frame.gray_image1, frame.gray_image2, show_image);
-            show_image.convertTo(show_image, CV_8UC1, 127, 127);
-            cv::imshow("gray", show_image);
-
-            cv::hconcat(frame.depth_image1, frame.depth_image2, show_image);
-            show_image.convertTo(show_image, CV_8UC1, 100);
-            cv::imshow("depth", show_image);
-
-            cv::hconcat(gradient_image_x, gradient_image_y, show_image);
-            show_image.convertTo(show_image, CV_32FC1, 1, 1);
-            show_image.convertTo(show_image, CV_8UC1, 127, 1);
-            cv::imshow("grad", show_image);
-
-            int key = cv::waitKey(0);
-            if (key == 'q')
-                return 0;
-        }
 
         // iterate direct-method
         for (int iteration = 0; iteration < 10; iteration++) {
             // A xi + B = 0
             cv::Mat1f A(cv::Mat1f::zeros(0, 6));
             cv::Mat1f B(cv::Mat1f::zeros(0, 1));
-            cv::Mat moved_image(frame.depth_image1.size(), CV_32FC1, Converter::INVALID);
+            cv::Mat moved_image(cur_frame.m_depth_image.size(), CV_32FC1, Converter::INVALID);
             float residual = 0;
 
             for (int col = 0; col < COL; col += 1) {
                 for (int row = 0; row < ROW; row += 1) {
                     cv::Point2f x_i0 = cv::Point2f(col, row);
-                    float depth1 = frame.depth_image1.at<float>(x_i0);
-                    float depth2 = frame.depth_image2.at<float>(x_i0);
+                    float depth1 = pre_frame.m_depth_image.at<float>(x_i0);
+                    float depth2 = cur_frame.m_depth_image.at<float>(x_i0);
                     if (depth2 < 0.001 or depth1 < 0.001)  //   1[mm]
                         continue;
 
                     // calc residual
-                    cv::Point2f x_i = Transform::warp(xi, x_i0, depth2, frame.intrinsic_depth);
+                    cv::Point2f x_i = Transform::warp(xi, x_i0, depth2, pre_frame.m_intrinsic);
                     if ((!math::isRange(x_i.x, 0, COL)) or (!math::isRange(x_i.y, 0, ROW)))
                         continue;
 
-                    float I_2 = Converter::getColorSubpix(frame.gray_image2, x_i);
-                    float I_1 = frame.gray_image1.at<float>(x_i0);
+                    float I_1 = pre_frame.m_gray_image.at<float>(x_i0);
+                    float I_2 = Converter::getColorSubpix(cur_frame.m_gray_image, x_i);
                     moved_image.at<float>(x_i0) = I_2;
-                    if (Converter::isInvalid(I_1) or Converter::isInvalid(I_2)) {
-                        // std::cout << I_1 << " " << I_2 << std::endl;
+                    if (Converter::isInvalid(I_1) or Converter::isInvalid(I_2))
                         continue;
-                    }
 
                     // calc jacobian
                     float gx = Converter::getColorSubpix(gradient_image_x, x_i);
                     float gy = Converter::getColorSubpix(gradient_image_y, x_i);
-                    if (Converter::isInvalid(gx) or Converter::isInvalid(gy)) {
-                        // std::cout << gx << " " << gy << std::endl;
+                    if (Converter::isInvalid(gx) or Converter::isInvalid(gy))
                         continue;
-                    }
 
                     float r = I_2 - I_1;
                     residual += r * r;
 
-                    cv::Mat1f jacobi1(cv::Mat1f::zeros(1, 2));
-                    jacobi1 = (cv::Mat1f(1, 2) << gx, gy);
-
-                    // if (math::isEpsilon(jacobi1(0)) or math::isEpsilon(jacobi1(1)))
-                    //     continue;
-
+                    cv::Mat1f jacobi1 = (cv::Mat1f(1, 2) << gx, gy);
                     cv::Mat1f jacobi2 = cv::Mat1f(cv::Mat1f::zeros(2, 6));
                     {
-                        cv::Point3f x_c = cv::Point3f(Transform::backProject(frame.intrinsic_depth, cv::Mat1f(x_i0), depth2));
-                        // std::cout << x_i0 << " " << x_c << std::endl;
-
+                        cv::Point3f x_c = cv::Point3f(Transform::backProject(cur_frame.m_intrinsic, cv::Mat1f(x_i0), depth2));
                         float x = x_c.x, y = x_c.y, z = x_c.z;
-                        float fx = frame.intrinsic_depth(0, 0);
-                        float fy = frame.intrinsic_depth(1, 1);
+                        float fx = cur_frame.m_intrinsic(0, 0);
+                        float fy = cur_frame.m_intrinsic(1, 1);
 
                         jacobi2(0, 0) = fx / z;
                         jacobi2(1, 0) = 0;
@@ -229,18 +140,34 @@ int main(int argc, char* argv[])
                 }
             }
 
-            moved_image.convertTo(moved_image, CV_8UC1, 127, 127);
-            cv::namedWindow("show", cv::WINDOW_NORMAL);
-            cv::imshow("show", moved_image);
-            int key = cv::waitKey(0);
-            if (key == 'q')
-                break;
+            // show image
+            {
+                cv::Mat show_image;
+                cv::hconcat(std::vector<cv::Mat>{pre_frame.m_gray_image, moved_image, cur_frame.m_gray_image}, show_image);
+                show_image.convertTo(show_image, CV_8UC1, 127, 127);
+                cv::imshow("gray", show_image);
+
+                cv::Mat show_image1;
+                cv::hconcat(pre_frame.m_depth_image, cur_frame.m_depth_image, show_image1);
+                show_image1.convertTo(show_image1, CV_8UC1, 100);
+
+                cv::Mat show_image2;
+                cv::hconcat(gradient_image_x, gradient_image_y, show_image2);
+                show_image2.convertTo(show_image2, CV_32FC1, 1, 1);
+                show_image2.convertTo(show_image2, CV_8UC1, 127, 1);
+                cv::vconcat(show_image1, show_image2, show_image);
+                cv::imshow("depth&grad", show_image);
+
+                int key = cv::waitKey(0);
+                if (key == 'q')
+                    return 0;
+            }
 
             // 最小二乗法を解く
             cv::Mat1f xi_update;
             cv::solve(A, -B, xi_update, cv::DECOMP_SVD);
             xi = math::se3::concatenate(xi, xi_update);
-            std::cout << "iteration: " << iteration << " r  : " << residual << " update " << cv::norm(xi_update) << std::endl;
+            std::cout << "iteration: " << iteration << " r : " << residual << " update " << cv::norm(xi_update) << " " << xi_update.t() << std::endl;
             residuals.push_back(residual);
             assert(math::testXi(xi));
 
@@ -252,6 +179,7 @@ int main(int argc, char* argv[])
 
     }  // level
 
+    // plot residuals
     namespace plt = matplotlibcpp;
     for (size_t i = 0; i < vector_of_residuals.size(); i++) {
         plt::subplot(1, vector_of_residuals.size(), i + 1);

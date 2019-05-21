c9ef1d9a4bb73d9539a6ef5f4ffbe0a9e089b6b2
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2019-05-21 12:58:19 +0900

    modify README

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Abstract
-* semi-dense-visual-odometryの再現実装
-* ただし，回転成分はないものと仮定する
-# TODO 
-* ストリーミングモードではキー入力で初期化できるようにする(Fri)
+## Description
+* [semi-dense-visual-odometry](https://jsturm.de/publications/data/engel2013iccv.pdf)の再現実装．
+* 2019年東京大学五月祭での展示用に作成．
+* ただし回転の追従がうまくいかず，展示時には回転運動が生じないように拘束して運用した
+    * for-exhibitionブランチはそのときのそれ
+## Demo
--- src/track/optimize.cpp
+++ src/track/optimize.cpp
@@ -4,44 +4,6 @@
 
 namespace Track
 {
-namespace
-{
-constexpr double e = std::numeric_limits<double>::epsilon();
-template <typename T = float>
-constexpr T sqrt(T s)
-{
-    T x = s / 2.0f;
-    T prev = 0.0f;
-
-    while (std::abs(x - prev) > e) {
-        prev = x;
-        x = (x + s / x) / 2.0f;
-    }
-    return x;
-}
-
-constexpr float d = 0.5f;
-constexpr float sqrt2d = sqrt(2.0f * d);
-constexpr float sqrtd2 = sqrt(d / 2.0f);
-constexpr float k0 = sqrtd2;
-constexpr float k1 = 0.5f / sqrtd2;
-constexpr float k2 = -1.0f / (sqrtd2 * d * 2.0f);
-
-// NOTE: huber関数の平方根
-inline float huber(float n)
-{
-    float a = std::abs(n);
-    if (a < d)
-        return n / sqrt2d;
-
-    // 2次近似
-    if (0 < n)
-        return k0 + (k1 + k2 * (a - d)) * (a - d);
-
-    return -k0 - (k1 + k2 * (a - d)) * (a - d);
-}
-}  // namespace
-
 // NOTE: ループ内部でメモリ確保すると遅い
 cv::Mat1f jacobi(cv::Mat1f::zeros(1, 6));
 
@@ -52,7 +14,7 @@ Outcome optimize(const Stuff& stuff)
 
     int max_size = stuff.cols * stuff.rows;
     int valid_pixels = 0;
-    cv::Mat1f A(cv::Mat1f::zeros(max_size, 3));
+    cv::Mat1f A(cv::Mat1f::zeros(max_size, 6));
     cv::Mat1f B(cv::Mat1f::zeros(max_size, 1));
 
     // TODO:
@@ -105,16 +67,15 @@ Outcome optimize(const Stuff& stuff)
             cv::Point3f x_c = Transform::backProject(stuff.K, x_i, depth);
             float x = x_c.x, y = x_c.y, z = x_c.z;
             float fgx = fx * gx, fgy = fy * gy;
-            // float xz = x / z, yz = y / z;
-            jacobi = cv::Mat1f::zeros(1, 3);
+            float xz = x / z, yz = y / z;
+            jacobi = cv::Mat1f::zeros(1, 6);
             jacobi(0) = fgx / z;
             jacobi(1) = fgy / z;
             jacobi(2) = -(fgx * x + fgy * y) / z / z;
-            // jacobi(3) = -fgx * xz * yz - fgy * (1.0f + yz * yz);
-            // jacobi(4) = fgx * (1.0f + xz * xz) + fgy * xz * yz;
-            // jacobi(5) = (-fgx * yz + fgy * xz);
+            jacobi(3) = -fgx * xz * yz - fgy * (1.0f + yz * yz);
+            jacobi(4) = fgx * (1.0f + xz * xz) + fgy * xz * yz;
+            jacobi(5) = (-fgx * yz + fgy * xz);
 
-            // float r = huber(I_2 - I_1);  // NOTE: huber関数の恩恵が少ない
             float r = I_2 - I_1;
             residual += r * r;
 
@@ -132,12 +93,8 @@ Outcome optimize(const Stuff& stuff)
         return Outcome{math::se3::xi(), -1, valid_pixels};
 
     // solve equation (A xi + B = 0)
-    cv::Mat1f xi_update, update;
-    cv::solve(A, -B, update, cv::DECOMP_SVD);
-    xi_update = cv::Mat1f::zeros(6, 1);
-    for (int i = 0; i < 3; i++) {
-        xi_update(i) = update(i);
-    }
+    cv::Mat1f xi_update;
+    cv::solve(A, -B, xi_update, cv::DECOMP_SVD);
     return Outcome{cv::Mat1f(-xi_update), residual / static_cast<float>(valid_pixels), valid_pixels};
 }
 
